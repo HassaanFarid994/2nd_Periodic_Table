0af7f004c593b2a863d6c941d84132ccf37d54ce
hassaan-hub <hassaanali994@gmail.com>
2025-06-10 14:02:58 +0500

    i use javacript

---
+++ app.js
@@ -0,0 +1,10 @@
+function Has(){
+    alert("Thankyou for The visiting My Periodic_Table")
+    alert("Give your feeback Thankyou")
+}
+
+
+function Maj(){
+    // alert("Thankyou for The visiting My Periodic_Table")
+    alert("Thankyou For Your Feedback")
+}
